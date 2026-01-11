0bdbd62c586268c7d1258060da971bcbf76bdbda
Pietro Santos Miranda <161655720+pietros133@users.noreply.github.com>
2026-01-10 21:13:43 -0300

    Replace single quotes with double quotes in index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const menuToggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
-const themeToggle = document.getElementById('theme-toggle');
+const menuToggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+const themeToggle = document.getElementById("theme-toggle");
 
-menuToggle.addEventListener('click', () => nav.classList.toggle('active'));
+menuToggle.addEventListener("click", () => nav.classList.toggle("active"));
 
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
